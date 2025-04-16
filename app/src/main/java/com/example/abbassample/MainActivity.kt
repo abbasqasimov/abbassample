2650d8d6fb1e3b4473b0060eabb2bf01ea77071e
abbasqasimov <qasimovabbas009@gmail.com>
2025-04-16 18:58:44 +0400

    update

--- app/src/main/java/com/example/abbassample/MainActivity.kt
+++ app/src/main/java/com/example/abbassample/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.example.abbassample
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.abbassample.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.google?.setOnClickListener {
+            val googleUrl="https://www.google.com"
+            val newIntent= Intent(Intent.ACTION_VIEW, Uri.parse(googleUrl))
+            startActivity(newIntent)
+
         }
+
+        binding?.facebook?.setOnClickListener {
+            val facebookUrl="https://www.facebook.com/"
+            val newIntent2=Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl))
+            startActivity(newIntent2)
+        }
+
+        binding?.apple?.setOnClickListener {
+            val appleUrl="https://www.apple.com/"
+            val newIntent3=Intent(Intent.ACTION_VIEW, Uri.parse(appleUrl))
+            startActivity(newIntent3)
+        }
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding=null
     }
 }
